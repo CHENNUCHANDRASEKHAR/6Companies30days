f81ae8db5df534abb0742a32060de63bb2b56900
Chennu Chandra sekhar <78692283+CHENNUCHANDRASEKHAR@users.noreply.github.com>
2023-01-24 21:50:23 +0530

    Day 9

---
+++ Adobe/12. Shortest Unsorted Continuous Subarray.java
@@ -0,0 +1,31 @@
+// Peoblem Link: https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
+
+// Language: Java
+
+
+
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+		int[] array = new int[nums.length];
+		for (int i = 0; i < nums.length; i++) 
+        {
+			array[i] = nums[i];
+		}
+		Arrays.sort(array);
+		int end = 0;
+		int start = nums.length;
+		for(int i = 0; i < nums.length; i++) 
+        {
+			if(array[i] != nums[i]) 
+            {
+				start = Math.min(start, i);
+				end = Math.max(end, i);
+			}
+		}
+		if (end-start > 0) 
+        {
+			return end-start+1;
+		}
+    	return 0;
+    }
+}
