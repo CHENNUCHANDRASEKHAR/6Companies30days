80f359d2a50c6ebf1fb4f9cbe696419e758c4ed0
Chennu Chandra sekhar <78692283+CHENNUCHANDRASEKHAR@users.noreply.github.com>
2023-01-24 21:26:06 +0530

    Day 5

---
+++ Goldman Sachs/15. Good People based on Statements.java
@@ -0,0 +1,43 @@
+// Problem Link: https://leetcode.com/problems/maximum-good-people-based-on-statements/
+
+// Language: Jav
+
+
+class Solution {
+    int max_num = 0;
+    
+    public int maximumGood(int[][] statements) {
+        dfs(statements, new boolean[statements.length], 0, 0);
+        return max_num;
+    }
+    
+    private boolean isValid(int[][] statements, boolean[] good, int k) {
+        for (int i = 0; i <= k; i++) {
+            for (int j = 0; j <= k; j++) {
+                
+                if (statements[i][j] == 1 && !good[j] && good[i]) return false;   
+                
+                if (statements[i][j] == 0 && good[j] && good[i]) return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private void dfs(int[][] statements, boolean[] good, int i, int count) {
+        if (i == good.length) {
+            max_num = Math.max(count, max_num);
+            return;
+        }
+        // try person i is good person
+        if (isValid(statements, good, i)) {
+            dfs(statements, good, i + 1, count);
+        }
+        good[i] = true;
+        if (isValid(statements, good, i)) {
+            dfs(statements, good, i + 1, count + 1);
+        }
+        good[i] = false;
+    }
+    
+}
