fc26d04d08cba42d34536d01905a49cfa2b80edd
Chennu Chandra sekhar <78692283+CHENNUCHANDRASEKHAR@users.noreply.github.com>
2023-01-25 22:28:18 +0530

    Day 18

---
+++ Google/7. K divisible Array Substrings.java
@@ -0,0 +1,26 @@
+// Problem Link: https://leetcode.com/problems/k-divisible-elements-subarrays/
+
+ // Language: Java
+
+
+class Solution {
+    public int countDistinct(int[] nums, int k, int p) {
+        int count = 0;
+     
+        Set<String> s = new HashSet<>();
+        for(int i = 0; i<nums.length; i++){
+              StringBuilder subarray = new StringBuilder();
+                count = 0;
+             for(int j = i; j<nums.length; j++){
+                subarray.append(nums[j] + ",");
+                 if(nums[j] % p == 0 )
+                     count++;
+                 if(count <= k)
+                     s.add(subarray.toString());
+             
+             }
+       
+        }
+        return s.size();
+    }
+}
