3f0e33724e5f70ad854a3cc48ae6adb00e79a4f0
Chennu Chandra sekhar <78692283+CHENNUCHANDRASEKHAR@users.noreply.github.com>
2023-01-27 01:09:36 +0530

    Day 21

---
+++ Amazon/1. Shuffle an array.java
@@ -0,0 +1,40 @@
+// Problem Link: https://leetcode.com/problems/shuffle-an-array/submissions/885868784/
+
+// Language: Java
+
+
+import java.util.Random;
+class Solution {
+    private int[] nums;
+    private Random random;
+    public Solution(int[] nums) {
+         this.nums = nums;
+        random = new Random();
+    }
+    
+    public int[] reset() {
+        return nums;
+    }
+    
+    public int[] shuffle() {
+        if(nums == null) return null;
+        int[] a = nums.clone();
+        for(int j = 1; j < a.length; j++) {
+            int i = random.nextInt(j + 1);
+            swap(a, i, j);
+        }
+        return a;
+    }
+     private void swap(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
