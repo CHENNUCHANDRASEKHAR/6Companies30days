f2fa84a8d96cbfb3c4852d261872f5cb2b01cf9b
Chennu Chandra sekhar <78692283+CHENNUCHANDRASEKHAR@users.noreply.github.com>
2023-01-30 21:41:05 +0530

    Day 25

---
+++ Amazon/13. Top K frequent words.java
@@ -0,0 +1,52 @@
+// Problem Link: https://leetcode.com/problems/top-k-frequent-words/
+
+// Language: Java
+
+
+
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+         HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String word: words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        
+        PriorityQueue<Item> queue = new PriorityQueue<>(new ItemComparator());
+        
+        for (String key: map.keySet()) {
+            queue.add(new Item(key, map.get(key)));
+        }
+        
+        List<String> list = new ArrayList<>();
+        
+        for (int i=0; i<k; i++) {
+            list.add(queue.remove().word);
+        }
+        
+        return list;
+    }
+}
+
+class Item {
+    String word;
+    int count;
+    
+    Item(String word, int count) {
+        this.word = word;
+        this.count = count;
+    }
+}
+
+class ItemComparator implements Comparator<Item> {
+    
+    public int compare(Item a, Item b) {
+        if (a.count < b.count) 
+            return 1;
+        else if (a.count > b.count)
+            return -1;
+        else
+            return a.word.compareTo(b.word);
+        
+    }
+}
